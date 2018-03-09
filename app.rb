8167400de3b0924e6064b1a4769451f279bf14d3
Brian Hurst <hursey013@gmail.com>
2018-03-08 22:02:03 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ require './lib/alexa/response'
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 
-OMIT_ACCOUNTS = [ '4V9AoqYZ35hX95eMbMmmFp0m74oDDESDo6O5j', 'Jej1pBwRnrcjaJJxjqB9tbxx6ewX1EuqQ3YJO' ].freeze
-OMIT_CATEGORIES = [ 'Transfer', 'Credit Card', 'Deposit', 'Payment' ].freeze
+OMIT_ACCOUNTS = [].freeze
+OMIT_CATEGORIES = [ 'Transfer', 'Credit', 'Deposit', 'Payment' ].freeze
 ACCOUNTS = ENV['ACCESS_TOKENS'].split(' ').freeze
 USERS = [ { name: 'Brian', percentage: 0.6 }, { name: 'Drew', percentage: 0.4 }].freeze
 
